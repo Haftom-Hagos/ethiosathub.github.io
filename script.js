d5e794ed8e978d09a3bded0046184578dccc8154
Haftom-Hagos <136759437+Haftom-Hagos@users.noreply.github.com>
2024-07-11 16:53:37 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -6,25 +6,48 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
+// Layers object to store the GeoJSON layers
+var layers = {
+    ethiopia: null,
+    zone: null,
+    region: null
+};
+
 // Function to load GeoJSON file
-function loadGeoJSON(url, styleOptions) {
+function loadGeoJSON(url, styleOptions, layerName) {
     fetch(url)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok ' + response.statusText);
-            }
-            return response.json();
-        })
+        .then(response => response.json())
         .then(data => {
-            console.log('GeoJSON data loaded successfully from ' + url);
-            L.geoJSON(data, {
+            layers[layerName] = L.geoJSON(data, {
                 style: styleOptions
-            }).addTo(map);
+            });
+            layers[layerName].addTo(map);
         })
         .catch(error => console.error('Error loading the GeoJSON file:', error));
 }
 
 // Load GeoJSON files
-loadGeoJSON('ethiopia.geojson', {color: 'blue', weight: 2});
-loadGeoJSON('eth_zone.geojson', {color: 'green', weight: 2});
-loadGeoJSON('eth_reg.geojson', {color: 'red', weight: 2});
+loadGeoJSON('ethiopia.geojson', {color: 'blue', weight: 2}, 'ethiopia');
+loadGeoJSON('eth_zone.geojson', {color: 'green', weight: 2}, 'zone');
+loadGeoJSON('eth_reg.geojson', {color: 'red', weight: 2}, 'region');
+
+// Function to toggle layers
+function toggleLayer(layerName, checked) {
+    if (checked) {
+        map.addLayer(layers[layerName]);
+    } else {
+        map.removeLayer(layers[layerName]);
+    }
+}
+
+// Event listeners for checkboxes
+document.getElementById('ethiopia').addEventListener('change', function(event) {
+    toggleLayer('ethiopia', event.target.checked);
+});
+
+document.getElementById('zone').addEventListener('change', function(event) {
+    toggleLayer('zone', event.target.checked);
+});
+
+document.getElementById('region').addEventListener('change', function(event) {
+    toggleLayer('
