e88d915304e29909aaf146370aa718bd2cb92b7c
Haftom-Hagos <136759437+Haftom-Hagos@users.noreply.github.com>
2024-07-12 10:16:25 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -10,7 +10,8 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 var layers = {
     ethiopia: null,
     zone: null,
-    region: null
+	region: null,
+    rivers: null
 };
 
 // Function to load GeoJSON file
@@ -30,6 +31,8 @@ function loadGeoJSON(url, styleOptions, layerName) {
 loadGeoJSON('ethiopia.geojson', {color: 'blue', weight: 2}, 'ethiopia');
 loadGeoJSON('eth_zone.geojson', {color: 'green', weight: 2}, 'zone');
 loadGeoJSON('eth_reg.geojson', {color: 'red', weight: 2}, 'region');
+loadGeoJSON('clipped_rivers_ethiopia.geojson', {color: 'red', weight: 2}, 'rivers');
+
 
 // Function to toggle layers
 function toggleLayer(layerName, checked) {
@@ -51,4 +54,6 @@ document.getElementById('zone').addEventListener('change', function(event) {
 
 document.getElementById('region').addEventListener('change', function(event) {
     toggleLayer('region', event.target.checked);
+document.getElementById('rivers').addEventListener('change', function(event) {
+    toggleLayer('rivers', event.target.checked);
 });
