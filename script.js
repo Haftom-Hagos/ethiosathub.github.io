9d5e07febe3ab81e392097d7504593b105fdbc8c
Haftom-Hagos <136759437+Haftom-Hagos@users.noreply.github.com>
2025-09-30 17:10:51 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-```javascript
+
 const BACKEND_URL = 'https://hafrepo-2.onrender.com'; // Render backend URL 
 
 let map, landcoverLayer, ndviLayer, drawnItems, selectedArea, selectedDistrict, selectedDistrictGeoJSON;
@@ -497,4 +497,5 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 });
-```
+
+
